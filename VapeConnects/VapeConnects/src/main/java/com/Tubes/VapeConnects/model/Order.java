37566da55176ff17912249179b2e1dae62cb7081
jorghameshal <rjorghameshal@gmail.com>
2025-12-11 02:49:37 +0700

    gabungin order

--- VapeConnects/VapeConnects/src/main/java/com/Tubes/VapeConnects/model/Order.java
+++ VapeConnects/VapeConnects/src/main/java/com/Tubes/VapeConnects/model/Order.java
@@ -41,7 +41,8 @@ public class Order {
     private String shippingPhone;
     private String shippingEmail;
 
-    private String status;
+    private String comment;
+
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
